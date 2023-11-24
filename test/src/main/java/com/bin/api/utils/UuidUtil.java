7d627c9e9ba09fc0c05b56fc360d6df8f05294ba
yanbin.yb@hotmail.com <jlyyanbin>
2023-11-24 20:12:48 +0800

    优化

--- test/src/main/java/com/bin/api/utils/UuidUtil.java
+++ test/src/main/java/com/bin/api/utils/UuidUtil.java
@@ -3,14 +3,20 @@ package com.bin.api.utils;
 import java.util.UUID;
 
 /**
- *
  * @author yanbin
  * @date 2017/7/1
  */
 public final class UuidUtil {
     public static String newUuidString() {
         UUID uuid = UUID.randomUUID();
-        return Long.toHexString(uuid.getMostSignificantBits())
+        String result = Long.toHexString(uuid.getMostSignificantBits())
                 + Long.toHexString(uuid.getLeastSignificantBits());
+        if (result.length() < 32) {
+            int len = 32 - result.length();
+            for (int i = 0; i < len; i++) {
+                result = result + "0";
+            }
+        }
+        return result;
     }
 }
